a78c6e1eca41c2acc03854b2cba72ec0079c7e39
mozuk <mozuk@PC4>
2026-04-09 17:55:21 +0900

    18時のコミット

---
+++ ch11/src/ch11/CleaningService.java
@@ -0,0 +1,10 @@
+package ch11;
+
+public interface CleaningService {
+	Shirt washShirt(Shirt s);
+
+	Towl washTowl(Towl t);
+
+	Coat washCoat(Coat c);
+
+}
---
+++ ch11/src/ch11/Creature11_13.java
@@ -0,0 +1,6 @@
+package ch11;
+
+public interface Creature11_13 {
+	void run();
+
+}
