db93083e2d4ac3962fd0e672e3f19527f73e14d9
mozuk <mozuk@PC4>
2026-05-12 18:02:12 +0900

    18時のコミット

--- moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -9,4 +9,10 @@ import com.example.moattravel.entity.House;
 public interface HouseRepository extends JpaRepository<House, Integer> {
 	//findAll(),getReferenceById(id),save(エンティティ),delete(エンティティ),deleteById(id)
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLike(String namekeyword, String addresskeyword, Pageable pageable);
+
+	public Page<House> findByAddressLike(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);//指定された値以下であるデータを検索
 }
