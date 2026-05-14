0ee61f43104ca79f6691fe692abb467005c568e6
mozuk <mozuk@PC4>
2026-05-14 12:46:11 +0900

    コミット

--- moattravel/src/main/java/com/example/moattravel/service/StripeService.java
+++ moattravel/src/main/java/com/example/moattravel/service/StripeService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 import com.example.moattravel.form.ReservationRegisterForm;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
-import com.stripe.model.billingportal.Session;
-import com.stripe.param.billingportal.SessionCreateParams;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 
 @Service
 public class StripeService {
@@ -18,8 +18,9 @@ public class StripeService {
 		Stripe.apiKey = "sk_test_51TWneCL1HIizIFvPeTbGBrndB9Q3JA5cXUou3vgeR8v05UvnnLjl94rVs3iBPuj6OwaUhzSVqVkN4OU4Yy2aCVb800gHiiNXN0";
 		String requestUrl = new String(httpServletRequest.getRequestURL());
 		SessionCreateParams params = SessionCreateParams.builder()
-				.addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-				.addLineItem(
+
+				.addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)//決済方法:クレカ
+				.addLineItem(//民宿名
 						SessionCreateParams.LineItem.builder()
 								.setPriceData(
 										SessionCreateParams.LineItem.PriceData.builder()
@@ -28,15 +29,15 @@ public class StripeService {
 																.setName(houseName)
 																.build())
 												.setUnitAmount((long) reservationRegisterForm.getAmount())
-												.setCurrency("jpy")
+												.setCurrency("jpy")//日本円
 												.build())
 								.setQuantity(1L)
 								.build())
-				.setMode(SessionCreateParams.Mode.PAYMENT)
-				.setSuccessUrl(
+				.setMode(SessionCreateParams.Mode.PAYMENT)//宿泊料金
+				.setSuccessUrl(//決済成功時の予約一覧ページ
 						requestUrl.replaceAll("/houses/[0-9]+/reservations/confirm", "") + "/reservations?reserved")
-				.setCancelUrl(requestUrl.replace("/reservations/confirm", ""))
-				.setPaymentIntentData(
+				.setCancelUrl(requestUrl.replace("/reservations/confirm", ""))//決済キャンセル時の民宿詳細ページ
+				.setPaymentIntentData(//予約情報をメタデータ（付随情報）として送信しておく
 						SessionCreateParams.PaymentIntentData.builder()
 								.putMetadata("houseId", reservationRegisterForm.getHouseId().toString())
 								.putMetadata("userId", reservationRegisterForm.getUserId().toString())
@@ -46,6 +47,7 @@ public class StripeService {
 								.putMetadata("amount", reservationRegisterForm.getAmount().toString())
 								.build())
 				.build();
+
 		try {
 			Session session = Session.create(params);
 			return session.getId();
