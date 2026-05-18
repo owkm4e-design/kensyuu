3115b38808000da83de6fdd6a17e218bcde3acc5
mozuk <o.wkm4e28@gmail.com>
2026-05-18 13:44:41 +0900

    コミット

--- moattravel-2/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel-2/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -1,5 +1,9 @@
 package com.example.moattravel.repository;
 
-public class HouseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel.entity.House;
+
+public interface HouseRepository extends JpaRepository<House, Integer> {//<エンティティのクラス型,主キーのデータ型>
 
 }
