91cb29c5e16f0667b7f7685ea1604eb70902d111
mozuk <o.wkm4e28@gmail.com>
2026-05-22 15:35:18 +0900

    コミット

--- moattravel-3/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel-3/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -1,13 +1,15 @@
 package com.example.moattravel.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel.entity.House;
 import com.example.moattravel.repository.HouseRepository;
@@ -23,13 +25,28 @@ public class AdminHouseController {
 	}
 
 	@GetMapping
-	public String index(Model model, Pageable pageable) {
-		List<House> houses = houseRepository.findAll();
-		Page<House> housePage = houseRepository.findAll(pageable);
+	public String index(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
 
-		model.addAttribute("houses", houses);
+		Page<House> housePage;
+
+		if (keyword != null && !keyword.isEmpty()) {
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		} else {
+			housePage = houseRepository.findAll(pageable);
+		}
+
 		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword", keyword);
 
 		return "admin/houses/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
+		House house = houseRepository.getReferenceById(id);
+		model.addAttribute("house", house);
+		return "admin/houses/show";
 	}
 }
