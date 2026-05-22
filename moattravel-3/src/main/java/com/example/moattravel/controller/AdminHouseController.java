116ab7ff621f4f3c3d160edc35bc42a328d8c6f3
mozuk <o.wkm4e28@gmail.com>
2026-05-22 12:24:23 +0900

    コミット

--- moattravel-3/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel-3/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -2,6 +2,8 @@ package com.example.moattravel.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,10 +23,12 @@ public class AdminHouseController {
 	}
 
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, Pageable pageable) {
 		List<House> houses = houseRepository.findAll();
+		Page<House> housePage = houseRepository.findAll(pageable);
 
 		model.addAttribute("houses", houses);
+		model.addAttribute("housePage", housePage);
 
 		return "admin/houses/index";
 	}
