352a768833ae3684ef5df4cf12d723b81303ad2f
mozuk <o.wkm4e28@gmail.com>
2026-05-26 19:01:46 +0900

    コミット

---
+++ moattravel-3/src/main/java/com/example/moattravel/service/StripeService.java
@@ -0,0 +1,58 @@
+package com.example.moattravel.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+
+import com.example.moattravel.form.ReservationRegisterForm;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.billingportal.Session;
+import com.stripe.param.billingportal.SessionCreateParams;
+
+@Service
+public class StripeService {
+
+	//セッションを作成し、Stripeに必要な情報を返す
+	public String createStripeSession(String housename, ReservationRegisterForm reservationRegisterForm,
+			HttpServletRequest httpServletRequest) {
+		Stripe.apiKey = "${API_SK}";
+		String requestUrl = new String(httpServletRequest.getRequestURL());
+		SessionCreateParams params = SessionCreateParams.builder()
+				.addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+				.addLineItem(SessionCreateParams.LineItem.builder()
+
+						.setPriceData(
+								SessionCreateParams.LineItem.PriceData.builder()
+										.setProductData(
+												SessionCreateParams.LineItem.PriceData.ProductData.builder()
+														.setName(houseName)
+														.build())
+										.setUnitAmount((long) reservationRegisterForm.getAmount())
+										.setCurrency("jpy")
+										.build())
+						.setQuantity(1L)
+						.build())
+				.setMode(SessionCreateParams.Mode.PAYMENT)
+				.setSuccessUrl(
+						requestUrl.replaceAll("/houses/[0-9]+/reservations/confirm", "") + "/reservations?reserved")
+				.setCancelUrl(requestUrl.replace("/reservations/confirm", ""))
+				.setPaymentIntentData(
+						SessionCreateParams.PaymentIntentData.builder()
+								.putMetadata("houseId", reservationRegisterForm.getHouseId().toString())
+								.putMetadata("userId", reservationRegisterForm.getUserId().toString())
+								.putMetadata("checkinDate", reservationRegisterForm.getCheckinDate())
+								.putMetadata("checkoutDate", reservationRegisterForm.getCheckoutDate())
+								.putMetadata("numberofPeople", reservationRegisterForm.getNumberOfPeople().toString())
+								.putMetadata("amount", reservationRegisterForm.getAmount().toString())
+								.build())
+				.build();
+		try {
+			Session session = Session.create(params);
+			return session.getId();
+		} catch (StripeException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
