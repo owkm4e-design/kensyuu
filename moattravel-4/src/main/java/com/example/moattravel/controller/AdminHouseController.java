a99b577923bb07ab5c1a330babec2e95161a8720
mozuk <o.wkm4e28@gmail.com>
2026-05-28 13:35:28 +0900

    コミット

--- moattravel-4/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel-4/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel.entity.House;
+import com.example.moattravel.form.HouseEditForm;
 import com.example.moattravel.form.HouseRegisterForm;
 import com.example.moattravel.repository.HouseRepository;
 import com.example.moattravel.service.HouseService;
@@ -61,8 +62,9 @@ public class AdminHouseController {
 		return "admin/houses/show";
 	}
 
-	@GetMapping("/register")
+	@GetMapping("/register") //GETリクエストが来たらこのメソッドを実行
 	public String register(Model model) {
+		//houseRegisterFormという名前でHouseRegisterFormオブジェクトをHTMLへ渡す
 		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
 
 		return "admin/houses/register";
@@ -71,17 +73,33 @@ public class AdminHouseController {
 	@PostMapping("/create")
 	public String create(@ModelAttribute HouseRegisterForm houseRegisterForm,
 			BindingResult bindingResult,
-			RedirectAttributes redirectAttributes) {
+			RedirectAttributes redirectAttributes) {//リダイレクト先にデータを渡すための機能を提供するインタフェース
 
 		if (bindingResult.hasErrors()) {
 			return "admin/houses/register";
 
 		}
 
-		houseService.create(houseRegisterForm);
+		houseService.create(houseRegisterForm);//登録処理
 
-		redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");
+		redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");//１回限り
 
 		return "redirect:/admin/houses";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable(name = "id") Integer id, Model model) {
+		House house = houseRepository.getReferenceById(id);
+
+		String imageName = house.getImageName();
+
+		HouseEditForm houseEditForm = new HouseEditForm(house.getId(), house.getName(), null, house.getDescription(),
+				house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(),
+				house.getPhoneNumber());
+
+		model.addAttribute("imageName", imageName);
+		model.addAttribute("houseEditForm", houseEditForm);
+
+		return "admin/houses/edit";
 	}
 }
