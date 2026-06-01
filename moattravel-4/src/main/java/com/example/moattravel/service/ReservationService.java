e0d09f5efa8a542bd3b732b50fdb6e9aa07a0f88
mozuk <o.wkm4e28@gmail.com>
2026-06-01 15:34:38 +0900

    コミット

--- moattravel-4/src/main/java/com/example/moattravel/service/ReservationService.java
+++ moattravel-4/src/main/java/com/example/moattravel/service/ReservationService.java
@@ -2,6 +2,7 @@ package com.example.moattravel.service;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Map;
 
 import jakarta.transaction.Transactional;
 
@@ -10,40 +11,45 @@ import org.springframework.stereotype.Service;
 import com.example.moattravel.entity.House;
 import com.example.moattravel.entity.Reservation;
 import com.example.moattravel.entity.User;
-import com.example.moattravel.form.ReservationRegisterForm;
 import com.example.moattravel.repository.HouseRepository;
 import com.example.moattravel.repository.ReservationRepository;
 import com.example.moattravel.repository.UserRepository;
 
 @Service
 public class ReservationService {
-	
+
 	private final ReservationRepository reservationRepository;
 	private final HouseRepository houseRepository;
 	private final UserRepository userRepository;
-	
-	public ReservationService(ReservationRepository reservationRepository,HouseRepository houseRepository,UserRepository userRepository) {
-		this.reservationRepository=reservationRepository;
-		this.houseRepository=houseRepository;
-		this.userRepository=userRepository;
-		
+
+	public ReservationService(ReservationRepository reservationRepository, HouseRepository houseRepository,
+			UserRepository userRepository) {
+		this.reservationRepository = reservationRepository;
+		this.houseRepository = houseRepository;
+		this.userRepository = userRepository;
+
 	}
-	
+
 	@Transactional
-	public void create(ReservationRegisterForm reservationRegisterForm) {
-		Reservation reservation=new Reservation();
-		House house=houseRepository.getReferenceById(reservationRegisterForm.getHouseId());
-		User user=userRepository.getReferenceById(reservationRegisterForm.getUserId());
-		LocalDate checkinDate=LocalDate.parse(reservationRegisterForm.getCheckinDate());
-		LocalDate checkoutDate=LocalDate.parse(reservationRegisterForm.getCheckoutDate());
-		
+	public void create(Map<String, String> paymentIntentObject) {
+		Reservation reservation = new Reservation();
+		Integer houseId = Integer.valueOf(paymentIntentObject.get("houseId"));
+		Integer userId = Integer.valueOf(paymentIntentObject.get("userId"));
+
+		House house = houseRepository.getReferenceById(houseId);
+		User user = userRepository.getReferenceById(userId);
+		LocalDate checkinDate = LocalDate.parse(paymentIntentObject.get("checkinDate"));
+		LocalDate checkoutDate = LocalDate.parse(paymentIntentObject.get("checkoutDate"));
+		Integer numberOfPeople = Integer.valueOf(paymentIntentObject.get("numberOfPeople"));
+		Integer amount = Integer.valueOf(paymentIntentObject.get("amount"));
+
 		reservation.setHouse(house);
 		reservation.setUser(user);
 		reservation.setCheckinDate(checkinDate);
 		reservation.setCheckoutDate(checkoutDate);
-		reservation.setNumberOfPeople(reservationRegisterForm.getNumberOfPeople());
-		reservation.setAmount(reservationRegisterForm.getAmount());
-		
+		reservation.setNumberOfPeople(numberOfPeople);
+		reservation.setAmount(amount);
+
 		reservationRepository.save(reservation);
 	}
 
