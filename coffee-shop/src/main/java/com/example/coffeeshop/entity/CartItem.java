150c9b5c8bb7d2aa2304691baae012acf6551476
mozuk <o.wkm4e28@gmail.com>
2026-06-09 17:07:45 +0900

    コミット

--- coffee-shop/src/main/java/com/example/coffeeshop/entity/CartItem.java
+++ coffee-shop/src/main/java/com/example/coffeeshop/entity/CartItem.java
@@ -4,11 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
-import org.springframework.data.annotation.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
