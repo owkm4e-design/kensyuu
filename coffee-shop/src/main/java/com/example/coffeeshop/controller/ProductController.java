7f5a47dea952dee1b18a46b3ad49e584b4177683
mozuk <o.wkm4e28@gmail.com>
2026-06-09 18:51:29 +0900

    komitto

---
+++ coffee-shop/src/main/java/com/example/coffeeshop/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.coffeeshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.coffeeshop.entity.Product;
+import com.example.coffeeshop.repository.ProductRepository;
+import com.example.coffeeshop.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+	private final ProductService productService;
+	private final ProductRepository repository;
+
+	@GetMapping("/{id}")
+	public String detail(@PathVariable(name = "id") Integer id, Model model) {
+		Product product = repository.getReferenceById(id);
+
+		model.addAttribute("product", product);
+		//model.addAttribute(, )
+
+		return "products/detail";
+	}
+}
