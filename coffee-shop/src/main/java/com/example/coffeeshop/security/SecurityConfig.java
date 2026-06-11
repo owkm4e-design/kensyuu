6c946f7cb5b20c9e5dec4140f1410ebabed17e06
mozuk <o.wkm4e28@gmail.com>
2026-06-12 08:53:47 +0900

    コミット

--- coffee-shop/src/main/java/com/example/coffeeshop/security/SecurityConfig.java
+++ coffee-shop/src/main/java/com/example/coffeeshop/security/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //Springの設定
-@EnableWebSecurity //セキュリティ機能
+@EnableWebSecurity //webセキュリティ機能
 @EnableMethodSecurity //メソッドごとのセキュリティ機能
 public class SecurityConfig {
 
