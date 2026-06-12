de6ca216473fe56761d103a16506d9871e8ec013
mozuk <o.wkm4e28@gmail.com>
2026-06-12 10:25:46 +0900

    Form

--- coffee-shop/src/main/java/com/example/coffeeshop/controller/AuthController.java
+++ coffee-shop/src/main/java/com/example/coffeeshop/controller/AuthController.java
@@ -1,6 +1,23 @@
 package com.example.coffeeshop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.coffeeshop.form.SignupForm;
+
+@Controller
 public class AuthController {
-	
+	@GetMapping("/login")
+	public String login() {
+		return "auth/login";
+	}
+
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("signupForm", new SignupForm());
+
+		return "auth/signup";
+	}
 
 }
